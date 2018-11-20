09b807239a078e6520977ae0bd0b79af3399b869
Rolando Mac <erolando.najera@gmail.com>
2018-11-20 07:56:22 -0600

    initial commit

---
+++ onb-usuarios/src/main/java/com/onboarding/api/usuarios/OnbUsuariosApplication.java
@@ -0,0 +1,12 @@
+package com.onboarding.api.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbUsuariosApplication.class, args);
+	}
+}
---
+++ onb-usuarios/src/test/java/com/onboarding/api/usuarios/OnbUsuariosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onboarding.api.usuarios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnbUsuariosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
